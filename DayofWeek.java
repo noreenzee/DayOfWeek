704f7f1494998f4892e569e7f74ad0022648897f
noreenzee <112978144+noreenzee@users.noreply.github.com>
2024-12-27 11:20:28 -0700

    Add files via upload

---
+++ DayofWeek.java
@@ -0,0 +1,48 @@
+
+package dayofweek;
+
+
+
+public class DayofWeek
+{
+  
+
+    
+    public static void main(String[] args)
+    {
+        int n;
+        n = 6;
+        if (n == 1)
+        {
+            System.out.println("Sunday");
+            
+        }
+        if (n ==2)
+        {
+            System.out.println("Monday");
+        }
+        if (n ==3)
+        {
+            System.out.println("Tuesday");
+        }
+        if (n ==4)
+        {
+            System.out.println("Wednesday");
+        }
+        if (n == 5)
+        {
+            System.out.println("Thursday");
+        }
+        if (n == 6)
+        {
+            System.out.println("Friday");
+        }
+        else if (n ==7)
+        {
+            System.out.println("Saturday");
+        }
+            
+        
+    }
+    
+}
